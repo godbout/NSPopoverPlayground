7e01912957df3e6c9d4f36ce4df0725d8e104df0
G <guill.bout@gmail.com>
2022-10-22 14:28:30 +0800

    Initial Commit

---
+++ NSPopoverPlayground/NSPopoverPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSPopoverPlaygroundApp.swift
+//  NSPopoverPlayground
+//
+//  Created by Guillaume Leclerc on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct NSPopoverPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
